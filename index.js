0d464a583f2ff2784e0d441554fb93399009ba56
Robert Killion <robertfkillion@gmail.com>
2021-05-06 23:45:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({},object,[key]: value);
+  var newObject = Object.assign({},object,{[key]: value});
   return newObject;
 }
